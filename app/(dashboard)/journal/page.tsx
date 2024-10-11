fb2880ca57f394c62cb5afd2fa5867c8b17be9a7
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-10-11 23:20:51 +0530

    dashboard

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,47 @@
+import EntryCard from '@/components/EntryCard'
+import NewEntryCard from '@/components/NewEntryCard'
+import Question from '@/components/Question'
+import { analyse } from '@/utils/ai'
+import { getUserFromClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import Link from 'next/link'
+
+const getEntries = async () => {
+  const user = await getUserFromClerkID()
+  const data = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      analysis: true,
+    },
+  })
+  //  await analyse(`yoo my cat died today. i 'm sooo happyyyy about it )`)
+  // console.log(data)
+  return data
+}
+
+const JournalPage = async () => {
+  const data = await getEntries()
+  // console.log(data)
+  return (
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h1 className="text-3xl mb-8">Journal</h1>
+      <div className="my-8">
+        <Question />
+      </div>
+      <div className="grid grid-cols-3 gap-4 p-10">
+        <NewEntryCard />
+        {data.map((entry) => (
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard key={entry.id} entry={entry} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+export default JournalPage
