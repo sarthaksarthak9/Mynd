db5d7d4f50f84c42aefc2ced0fae609057b1f5f2
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-10-11 23:21:50 +0530

    api

---
+++ app/api/journal/route.ts
@@ -0,0 +1,31 @@
+import { analyse } from "@/utils/ai"
+import { getUserFromClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+import { revalidatePath } from "next/cache"
+import { NextResponse } from "next/server"
+
+export const POST = async () => {
+    const user = await getUserFromClerkID()
+    const entry = await prisma.journalEntry.create({
+        data: {
+            userId: user.id,
+            content: "write about you day here",
+        },
+    })
+
+    // console.log("Before analysis, entry:", entry);
+    const analysis = await analyse(entry.content);
+    console.log("Analyzing entry:", entry.content);
+
+
+    await prisma.analysis.create({
+        data: {
+            userId: user.id,
+            entryId: entry.id,
+            ...analysis,
+        }
+    })
+
+    revalidatePath("/journal")
+    return NextResponse.json({ data: entry })
+}
