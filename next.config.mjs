b7816e81b3a6c27e998ff84dfd4833fd3a47119b
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-10-31 23:23:18 +0530

    config

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreBuildErrors: true,
+    ignoreDuringBuilds: true,
+  },
+}
+
+export default nextConfig
